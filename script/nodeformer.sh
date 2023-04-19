c82a66f3b5417a52bf46bda9942de79d2eed86a8
zrh-LegionLinux <11912614@mail.sustech.edu.cn>
2023-04-19 20:52:08 +0800

    add nodeformer training script

---
+++ script/nodeformer.sh
@@ -0,0 +1,5 @@
+# Please run in the root directory
+
+# proteins
+python nodeformer/main_nodeformer.py --dataset ogbn-proteins --metric rocauc --method nodeformer_encoder --lr 1e-2 --weight_decay 0. --num_layers 3 --hidden_channels 64 --num_heads 1 --rb_order 1 --rb_trans identity --lamda 0.1 --M 50 --K 5 --use_bn --use_residual --use_gumbel --use_act --use_jk --batch_size 5000 --runs 5 --epochs 1500 --eval_step 9 --device 0 --num_parts 5 --shuffle  --save_model
+
