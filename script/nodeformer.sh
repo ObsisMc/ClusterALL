c61d9258cd83740afa58003efdb3aa0a9ec4ecb6
zrh-LegionLinux <11912614@mail.sustech.edu.cn>
2023-05-10 14:12:22 +0800

    update training script

--- script/nodeformer.sh
+++ script/nodeformer.sh
@@ -1,7 +1,10 @@
 # Please run in the root directory
+# original
+python nodeformer/main-batch.py --dataset ogbn-arxiv --metric acc --method nodeformer --lr 1e-2 --weight_decay 0. --num_layers 3 --hidden_channels 64 --num_heads 1 --rb_order 1 --rb_trans identity --lamda 0.1 --M 50 --K 5 --use_bn --use_residual --use_gumbel --use_act --use_jk --batch_size 20000 --runs 10 --epochs 1000 --eval_step 9 --device 0  --save_model
+python nodeformer/main-batch.py --dataset ogbn-proteins --metric rocauc --method nodeformer --lr 1e-2 --weight_decay 0. --num_layers 3 --hidden_channels 64 --num_heads 1 --rb_order 1 --rb_trans identity --lamda 0.1 --M 50 --K 5 --use_bn --use_residual --use_gumbel --use_act --use_jk --batch_size 20000 --runs 5 --epochs 1000 --eval_step 9 --device 0 --save_model
 
 # proteins
-python nodeformer/main_nodeformer.py --dataset ogbn-proteins --metric rocauc --method nodeformer_encoder --lr 1e-2 --weight_decay 0. --num_layers 3 --hidden_channels 64 --num_heads 1 --rb_order 1 --rb_trans identity --lamda 0.1 --M 50 --K 5 --use_bn --use_residual --use_gumbel --use_act --use_jk --batch_size 5000 --runs 5 --epochs 1500 --eval_step 9 --device 0 --num_parts 5 --shuffle  --save_model
+python nodeformer/main_nodeformer.py --dataset ogbn-proteins --metric rocauc --method nodeformer_encoder --lr 1e-2 --weight_decay 0. --num_layers 3 --hidden_channels 64 --num_heads 1 --rb_order 1 --rb_trans identity --lamda 0.1 --M 50 --K 5 --use_bn --use_residual --use_gumbel --use_act --use_jk --batch_size 20000 --runs 5 --epochs 1500 --eval_step 9 --device 0 --num_parts 10 --shuffle  --save_model --epoch_gap 199 --dropout_cluster 0
 
 # arxiv
-python nodeformer/main_nodeformer.py --dataset ogbn-arxiv --metric acc --method nodeformer_encoder --lr 1e-2 --weight_decay 0. --num_layers 3 --hidden_channels 64 --num_heads 1 --rb_order 1 --rb_trans identity --lamda 0.1 --M 50 --K 5 --use_bn --use_residual --use_gumbel --use_act --use_jk --batch_size 10000 --runs 5 --epochs 1000 --eval_step 9 --device 0 --num_parts 50 --shuffle  --save_model
+python nodeformer/main_nodeformer.py --dataset ogbn-arxiv --metric acc --method nodeformer_encoder --lr 1e-2 --weight_decay 0. --num_layers 3 --hidden_channels 64 --num_heads 1 --rb_order 1 --rb_trans identity --lamda 0.1 --M 50 --K 5 --use_bn --use_residual --use_gumbel --use_act --use_jk --batch_size -1 --runs 5 --epochs 1200 --eval_step 9 --device 0 --num_parts 3 --shuffle  --save_model --epoch_gap 99 --dropout_cluster 0
